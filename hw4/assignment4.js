28b07881aa7858ea199b5710d5e5ad1027b98744
hittecd <hittecd@gmail.com>
2018-02-19 17:47:03 -0700

    implemented beta alert

--- hw4/assignment4.js
+++ hw4/assignment4.js
@@ -3,6 +3,22 @@ $("#cat_picker_form").submit(
         event.preventDefault();
         var pickedCat = $('input[name=catPicker]:checked').val();
 
-        alert("You selected " + pickedCat + " as your favorite cat. Good Choice!");
+        $("#alert_msg").text("You selected " + pickedCat + " as your favorite cat. Good Choice!")
+        $("#alert_container").show();
     }
+);
+
+$(".navbar-btn").hover(
+    function () {
+        $(this).addClass("hover");
+    },
+    function () {
+        $(this).removeClass("hover")
+    }
+);
+
+$("#confirmation_btn").click(
+  function() {
+      $("#alert_container").hide();
+  }
 );
